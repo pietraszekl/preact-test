f96a6ae05cd3360961164ddc31fe17c0f3003304
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-06-17 10:31:41 +0100

    Added User component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import { h } from "preact";
+import User from "./User";
+
+const users = [
+  {
+    image: "https://avatars0.githubusercontent.com/u/525704?v=3&s=460",
+    name: "Lukasz Pietraszek"
+  }
+];
+
 export function App() {
   return (
     <div class="app">
-      <h1>Hello word!</h1>
+      {users.map(user => <User {...user} key={user.name} />)}
     </div>
   );
 }
